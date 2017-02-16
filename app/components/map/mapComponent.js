6884169ba0aa4ebdb8446b59b93d5efab07df42b
Laurent MARCHAND <e_lau@hotmail.fr>
2017-02-16 19:18:09 +0100

    Structure

---
+++ app/components/map/mapComponent.js
@@ -0,0 +1,6 @@
+'use strict'
+angular.module("map")
+	.component("map", {
+		templateUrl: "/app/components/map/mapTemplate.html",
+		controller: "mapController"
+	})
---
+++ app/components/map/mapController.js
@@ -0,0 +1,4 @@
+angular.module("map")
+  .controller("mapController", ["$http", function mapController($http) {
+
+  }])
